99ee52b1dc60f2ee2da05def29ea8d83b368395f
M. Yoga Nugraha <myoganugraha@gmail.com>
2017-04-12 16:12:23 +0800

    aw

---
+++ Nyoba Javascript/nyoba javascript.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+function sayHello() {
+	"use strict";
+   alert(<div class="modal-body">
+        <form action="mailto:10151026@itk.ac.id" method="post" enctype="text/plain">
+			Name:<br>
+			<input type="text" name="name"><br>
+			E-mail:<br>
+			<input type="text" name="mail"><br>
+			Comment:<br>
+			<input type="text" name="comment" size="50"><br><br>
+			<input type="submit" value="Send">
+			<input type="reset" value="Reset">
+		</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    			</div><!-- /.modal-content -->
+  			</div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->)
+}
+			
+function makanDong(){
+   "use strict";
+   alert("Hubungin ali Husni Penjual Pisang")
+}
+
